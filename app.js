92ce6592399876f060923d666819d9ee860c02f0
Samuraiihot <nanaharashuya2@gmail.com>
2018-11-12 18:11:03 +0700

    Update Code

--- app.js
+++ app.js
@@ -8,12 +8,7 @@ const regis_1 = require('./tms_App/tms_registstep_1');
 const regis_2 = require('./tms_App/tms_registstep_2');
 const regis_3 = require('./tms_App/tms_registstep_3');
 
-//-------Modify by SamuraiiHot 2018-10-31
-const cors=require('cors')
-// app.use(cors())
-const web_api=require("./tms_api_web/routes")
 
-//-------Modify by SamuraiiHot 2018-10-31
 
 
 ////Body parser 
@@ -24,12 +19,12 @@ app.use(bodyParser.urlencoded({ extended:true,limit:1024*1024*20,type:'applicati
 app.use(bodyParser.json({limit:1024*1024*2000, type:'application/json'}));
 
 //-------Modify by SamuraiiHot 2018-10-31
-//const cors=require('cors')
+const cors=require('cors')
 app.use(cors())
-// const web_api=require("./tms_api_web/routes")
- app.use("/web-api",web_api)
-//const app_api=require("./tms_App/routes")
-//app.use("/app-api",app_api)
+const web_api=require("./tms_api_web/routes")
+app.use("/web-api",web_api)
+const app_api=require("./tms_App/routes")
+app.use("/app-api",app_api)
 //-------Modify by SamuraiiHot 2018-10-31
 
 app.use('/show_tsc',show_tsc);
