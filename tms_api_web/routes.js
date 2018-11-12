3db8e5245f3e3d259d1b91725cf66ea14be135bf
Samuraiihot <nanaharashuya2@gmail.com>
2018-11-12 18:16:28 +0700

    Update code

--- tms_api_web/routes.js
+++ tms_api_web/routes.js
@@ -4,26 +4,16 @@ const DPLT_ADDP_DplusSystem_User = require("./DPLT_ADDP_DplusSystem_User")
 const TMS_Special_Circles =require("./TMS_Special_Circles")
 
 router.get("/login/:id&:pass", (req, resp) => {
-    console.log("login");
     var id = req.params.id, pass = req.params.pass
     DPLT_ADDP_DplusSystem_User.model.find_by_id_pass(id, pass, (res_data) => {
         resp.json(res_data)
     })
 })
-router.post('/special-circles',function(req,resp){
-    let tms_doc = req.body.tms_doc;
-    let invoice = req.body.invoice;
-
-    async function main(){ 
-        resp.status(500).json({
-            result: tms_doc,
-            status:500
-       });
-       
-    }
-    main()
-
-
+router.post("/special-circles/add-task/",(req,resp)=>{
+    TMS_Special_Circles.model.create_tsc_one(req.body[0],(res_data)=>{
+        //------------Commit
+        resp.json(res_data)
+    })
 })
 
 module.exports = router
