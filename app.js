942c9dbf4c501bca49567d01336f17f66a38b4a5
birdpot1234 <Peerapon1234>
2018-11-12 16:25:09 +0700

    API kerry

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const show_tsc = require('./api/tsc/show_tsc');
 const regis_1 = require('./tms_App/tms_registstep_1');
 const regis_2 = require('./tms_App/tms_registstep_2');
 const regis_3 = require('./tms_App/tms_registstep_3');
-
+const info    = require('./kerry_api/info');
+const update_status = require('./kerry_api/update_status');
 //-------Modify by SamuraiiHot 2018-10-31
 const cors=require('cors')
 // app.use(cors())
@@ -37,6 +38,8 @@ app.use('/',regis_1);
 app.use('/',regis_2);
 app.use('/',regis_3);
 app.use("/",web_api)
+app.use("/",info);
+app.use("/",update_status);
 app.get('/',(req,res)=>{
     res.render('index');
 })
