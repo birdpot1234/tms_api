bb6609357175d4cc90d8097aeef43123933daee3
Samuraiihot <nanaharashuya2@gmail.com>
2018-11-23 12:29:24 +0700

    Update 11/23/2018 by  SamuraiiHot

--- tms_App/routes.js
+++ tms_App/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Messenger = require("./Messenger")
+const TMS_Special_Circles = require("../tms_api_web/TMS_Special_Circles")
 // const TMS_Special_Circles =require("./TMS_Special_Circles")
 
 router.get("/login/:token", (req, resp) => {
@@ -11,6 +12,11 @@ router.get("/login/:token", (req, resp) => {
 })
 router.get("/get-messenger/",(req,resp)=>{
     Messenger.model.find_no_admin((res_data) => {
+        resp.json(res_data)
+    })
+})
+router.put("/tsc/update-status/",(req,resp)=>{
+    TMS_Special_Circles.model.update_status(req.body[0],(res_data)=>{
         resp.json(res_data)
     })
 })
--- tms_api_web/TMS_Special_Circles.js
+++ tms_api_web/TMS_Special_Circles.js
@@ -1,9 +1,66 @@
 var { dbConnectData_TransportApp } = require('../connect_sql')
-const { Gen_Document5digit, save_log,server_response } = require("../service")
+const { Gen_Document5digit, save_log, server_response } = require("../service")
 const moment = require('moment')
 var sql = require('mssql')
 
 const model = {
+    update_status(data, callback) {
+        let { status, tsc_document, invoice, car_type, staff1, staff2, staff3, zone, trip, messenger_code, messenger_name } = data
+        sql.close()
+        const pool = new sql.ConnectionPool(dbConnectData_TransportApp)
+        pool.connect(err => {
+            if (err) {
+                save_log("Connection is close", "update_status", "TMS_Special_Circles", "No connection")
+                callback(server_response(500, "Connection is close", ""))
+            }
+            const transaction = new sql.Transaction(pool)
+            transaction.begin(err => {
+                if (err) {
+                    save_log("Connection is close", "update_status", "TMS_Special_Circles", "No connection")
+                    callback(server_response(500, "Connection is close", ""))
+                }
+                let rolledBack = false
+                transaction.on("rollback", aborted => {
+                    rolledBack = true
+                })
+                var req = new sql.Request(transaction)
+                var sql_query = "UPDATE TMS_Special_Circles SET \
+                status="+ status + ",\
+                messenger_code='"+ messenger_code + "',\
+                messenger_name='"+ messenger_name + "',\
+                car_type='"+ car_type + "',\
+                shipment_staff_1='"+ staff1 + "',\
+                shipment_staff_2='"+ staff2 + "',\
+                shipment_staff_3='"+ staff3 + "',\
+                trip='"+ trip + "',\
+                Zone='"+ zone + "' \
+                WHERE (tsc_document LIKE '"+ tsc_document + "')"
+                req.query(sql_query, (err, result) => {
+                    if (err) {
+                        if (!rolledBack) {
+                            transaction.rollback(err => {
+                                if (err) {
+                                    pool.close()
+                                    save_log(result, "update_status", "TMS_Special_Circles", data)
+                                    callback(server_response(501, "Error query SQL", err))
+                                }
+                            })
+                        }
+                    } else {
+                        transaction.commit(err => {
+                            pool.close()
+                            if (err) {
+                                save_log(result, "update_status", "TMS_Special_Circles", data)
+                                callback(server_response(501, "Error query SQL", err))
+                            }
+                            save_log(result, "update_status", "TMS_Special_Circles", data)
+                            callback(server_response(200, "Success", result.rowsAffected))
+                        })
+                    }
+                })
+            })
+        })
+    },
     gen_tsc_document(callback) {
         sql.close()
         const pool = new sql.ConnectionPool(dbConnectData_TransportApp)
@@ -18,12 +75,12 @@ const model = {
                 pool.close()
                 if (result.recordset.length > 0) {
                     Gen_Document5digit("TSC", result.recordset[0].tsc_document, (res_data) => {
-                        save_log(res_data,"gen_tsc_document","TSC",res_data)
+                        save_log(res_data, "gen_tsc_document", "TSC", res_data)
                         callback(res_data)
                     })
                 } else {
                     Gen_Document5digit("TSC", "", (res_data) => {
-                        save_log(res_data,"gen_tsc_document","TSC",res_data)
+                        save_log(res_data, "gen_tsc_document", "TSC", res_data)
                         callback(res_data)
                     })
                 }
@@ -32,56 +89,56 @@ const model = {
             });
         })
     },
-    create_tsc_one(data,callback) {
+    create_tsc_one(data, callback) {
         this.gen_tsc_document((res_data) => {
             //------กำหนดค่าให้กับตัวแปรตามฟิลด์
             var tsc_document = res_data
             var create_date = moment().format("YYYY-MM-DD H:m:s")
-            var user_request_code=data.user_request_code
-            var user_request_name=data.user_request_name
-            var user_request_department=data.user_request_department
-            var user_request_tel=data.user_request_tel
-            var receive_from=data.receive_from
-            var receive_date=data.receive_date
-            var receive_time_first=data.receive_time_first
-            var receive_time_end=data.receive_time_end
-            var send_to=data.send_to
-            var send_date=data.send_date
-            var send_time_first=data.send_time_first
-            var send_time_end=data.send_time_end
-            var send_tel=data.send_tel
-            var task_group=data.task_group
-            var task_group_document=data.task_group_document
-            var task_group_amount=data.task_group_amount
-            var task_group_quantity=data.task_group_quantity
-            var task_group_pic=data.task_group_pic
-            var comment=data.comment
-            var work_type=data.work_type
-            var status=data.status
-            var messenger_code=data.messenger_code
-            var messenger_name=data.messenger_name
-            var car_type=data.car_type
-            var shipment_staff_1=data.shipment_staff_1
-            var shipment_staff_2=data.shipment_staff_2
-            var messenger_comment=data.messenger_comment
-            var task_detail=data.task_detail
-            var status_finish=data.status_finish
-            var customerID=data.customerID
-            var customerName=data.customerName
-            var Zone=data.Zone
-            var address_shipment=data.address_shipment
-            var detail_cn=data.detail_cn
+            var user_request_code = data.user_request_code
+            var user_request_name = data.user_request_name
+            var user_request_department = data.user_request_department
+            var user_request_tel = data.user_request_tel
+            var receive_from = data.receive_from
+            var receive_date = data.receive_date
+            var receive_time_first = data.receive_time_first
+            var receive_time_end = data.receive_time_end
+            var send_to = data.send_to
+            var send_date = data.send_date
+            var send_time_first = data.send_time_first
+            var send_time_end = data.send_time_end
+            var send_tel = data.send_tel
+            var task_group = data.task_group
+            var task_group_document = data.task_group_document
+            var task_group_amount = data.task_group_amount
+            var task_group_quantity = data.task_group_quantity
+            var task_group_pic = data.task_group_pic
+            var comment = data.comment
+            var work_type = data.work_type
+            var status = data.status
+            var messenger_code = data.messenger_code
+            var messenger_name = data.messenger_name
+            var car_type = data.car_type
+            var shipment_staff_1 = data.shipment_staff_1
+            var shipment_staff_2 = data.shipment_staff_2
+            var messenger_comment = data.messenger_comment
+            var task_detail = data.task_detail
+            var status_finish = data.status_finish
+            var customerID = data.customerID
+            var customerName = data.customerName
+            var Zone = data.Zone
+            var address_shipment = data.address_shipment
+            var detail_cn = data.detail_cn
 
             sql.close()
             const pool = new sql.ConnectionPool(dbConnectData_TransportApp)
             pool.connect(err => {
                 if (err) {
-                    save_log("Connection is close","create_tsc_one","TMS_Special_Circles","No connection")
-                    callback(server_response(500,"Connection is close",""))
+                    save_log("Connection is close", "create_tsc_one", "TMS_Special_Circles", "No connection")
+                    callback(server_response(500, "Connection is close", ""))
                 }
                 const transaction = new sql.Transaction(pool)
                 transaction.begin(err => {
-                    if (err) callback(server_response(500,"Connection is close",""));
+                    if (err) callback(server_response(500, "Connection is close", ""));
                     let rolledBack = false
                     transaction.on("rollback", aborted => {
                         rolledBack = true
@@ -126,60 +183,60 @@ const model = {
                      ,[address_shipment] \
                      ,[detail_cn])\
                      VALUES \
-                     ( '"+ tsc_document +"'\
-                     ,'"+create_date+"'\
-                     ,'"+user_request_code+"'\
-                     ,'"+user_request_name+"'\
-                     ,'"+user_request_department+"'\
-                     ,'"+user_request_tel+"'\
-                     ,'"+receive_from+"'\
-                     ,'"+receive_date+"'\
-                     ,'"+receive_time_first+"'\
-                     ,'"+receive_time_end+"'\
-                     ,'"+send_to+"'\
-                     ,'"+send_date+"'\
-                     ,'"+send_time_first+"'\
-                     ,'"+send_time_end+"'\
-                     ,'"+send_tel+"'\
-                     ,'"+task_group+"'\
-                     ,'"+task_group_document+"'\
-                     ,'"+task_group_amount+"'\
-                     ,'"+task_group_quantity+"'\
-                     ,'"+task_group_pic+"'\
-                     ,'"+comment+"'\
-                     ,'"+work_type+"'\
-                     ,'"+status+"'\
-                     ,'"+messenger_code+"'\
-                     ,'"+messenger_name+"'\
-                     ,'"+car_type+"'\
-                     ,'"+shipment_staff_1+"'\
-                     ,'"+shipment_staff_2+"'\
-                     ,'"+messenger_comment+"'\
-                     ,'"+task_detail+"'\
-                     ,'"+status_finish+"'\
-                     ,'"+customerID+"'\
-                     ,'"+customerName+"'\
-                     ,'"+Zone+"'\
-                     ,'"+address_shipment+"'\
-                     ,'"+detail_cn+"' )"
+                     ( '"+ tsc_document + "'\
+                     ,'"+ create_date + "'\
+                     ,'"+ user_request_code + "'\
+                     ,'"+ user_request_name + "'\
+                     ,'"+ user_request_department + "'\
+                     ,'"+ user_request_tel + "'\
+                     ,'"+ receive_from + "'\
+                     ,'"+ receive_date + "'\
+                     ,'"+ receive_time_first + "'\
+                     ,'"+ receive_time_end + "'\
+                     ,'"+ send_to + "'\
+                     ,'"+ send_date + "'\
+                     ,'"+ send_time_first + "'\
+                     ,'"+ send_time_end + "'\
+                     ,'"+ send_tel + "'\
+                     ,'"+ task_group + "'\
+                     ,'"+ task_group_document + "'\
+                     ,'"+ task_group_amount + "'\
+                     ,'"+ task_group_quantity + "'\
+                     ,'"+ task_group_pic + "'\
+                     ,'"+ comment + "'\
+                     ,'"+ work_type + "'\
+                     ,'"+ status + "'\
+                     ,'"+ messenger_code + "'\
+                     ,'"+ messenger_name + "'\
+                     ,'"+ car_type + "'\
+                     ,'"+ shipment_staff_1 + "'\
+                     ,'"+ shipment_staff_2 + "'\
+                     ,'"+ messenger_comment + "'\
+                     ,'"+ task_detail + "'\
+                     ,'"+ status_finish + "'\
+                     ,'"+ customerID + "'\
+                     ,'"+ customerName + "'\
+                     ,'"+ Zone + "'\
+                     ,'"+ address_shipment + "'\
+                     ,'"+ detail_cn + "' )"
 
                     req.query(sql_query, (err, result) => {
                         if (err) {
                             if (!rolledBack) {
                                 transaction.rollback(err => {
-                                    if (err) callback(server_response(501,"Error query SQL",err));
+                                    if (err) callback(server_response(501, "Error query SQL", err));
                                     pool.close()
-                                    console.log("sql_query",sql_query)
-                                    save_log(result,"create_tsc_one","TMS_Special_Circles",data)
-                                    callback(server_response(501,"Error query SQL",err))
+                                    // console.log("sql_query", sql_query)
+                                    save_log(result, "create_tsc_one", "TMS_Special_Circles", data)
+                                    callback(server_response(501, "Error query SQL", err))
                                 })
                             }
                         } else {
                             transaction.commit(err => {
                                 pool.close()
-                                if (err) callback(server_response(501,"Error query SQL",err));
-                                save_log(result,"create_tsc_one","TMS_Special_Circles",data)
-                                callback(server_response(200,"Success",result.recordset))
+                                if (err) callback(server_response(501, "Error query SQL", err));
+                                save_log(result, "create_tsc_one", "TMS_Special_Circles", data)
+                                callback(server_response(200, "Success", result.recordset))
                             })
                         }
                     })
